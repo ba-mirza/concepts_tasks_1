655625bdacb701349c8991414bb633353bf2e16a
dev <almas1610@gmail.com>
2025-06-27 19:24:10 +0500

    [updated]: Default values, cloning and copying

--- default_clone_copy_1/src/main.rs
+++ default_clone_copy_1/src/main.rs
@@ -7,6 +7,25 @@ struct Point {
     y: f64,
 }
 
+#[derive(Debug)]
+struct Polyline {
+    points: Vec<Point>,
+}
+
+impl Polyline {
+    fn new(x: f64, y: f64) -> Self {
+        Self { points: vec![Point{x, y}] }
+    }
+}
+
+impl Clone for Polyline {
+    fn clone(&self) -> Self {
+        Polyline {
+            points: self.points.clone(),
+        }
+    }
+}
+
 impl Default for Point {
     fn default() -> Self {
         Self { x: 1.0, y: 1.0 }
@@ -28,6 +47,13 @@ impl Copy for Point {}
 fn main() {
     let p1 = Point { x: 3.0, y: 2.0 };
     let p2 = p1;
+
+    let mut polyline = Polyline::new(0.0, 0.0);
+    polyline.points.push(p1.clone());
+    let pl2 = polyline.clone();
+
+    println!("polyline: {:?}", pl2);
+    println!("polyline: {:?}", polyline);
 
     println!("p1: {:?}", p1);
     println!("p2: {:?}", p2);
