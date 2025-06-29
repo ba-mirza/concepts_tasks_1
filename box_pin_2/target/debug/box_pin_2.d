5d94d6e80dd6ef5ebbacf03eea5c09c4621f65dc
dev <almas1610@gmail.com>
2025-06-29 22:42:26 +0500

    [task_2]: box_pin_2

---
+++ box_pin_2/target/debug/box_pin_2.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/almas/OneDrive/Desktop/lowlvl/tasks_1/box_pin_2/target/debug/box_pin_2: /mnt/c/Users/almas/OneDrive/Desktop/lowlvl/tasks_1/box_pin_2/src/main.rs
---
+++ box_pin_2/target/debug/deps/box_pin_2-ba708850ee2598d6.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/almas/OneDrive/Desktop/lowlvl/tasks_1/box_pin_2/target/debug/deps/box_pin_2-ba708850ee2598d6: src/main.rs
+
+/mnt/c/Users/almas/OneDrive/Desktop/lowlvl/tasks_1/box_pin_2/target/debug/deps/box_pin_2-ba708850ee2598d6.d: src/main.rs
+
+src/main.rs:
