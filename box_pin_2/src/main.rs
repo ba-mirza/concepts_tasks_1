1c82a13a66dc6af5f97203916dcbda88880d046f
dev <almas1610@gmail.com>
2025-06-30 06:56:09 +0500

    [task_2]: Implemented trait for Box<T> type

--- box_pin_2/src/main.rs
+++ box_pin_2/src/main.rs
@@ -1,3 +1,20 @@
+use core::fmt;
+use std::pin::Pin;
+
+trait SayHi: fmt::Debug {
+    fn say_hi(self: Pin<&Self>) {
+        println!("Hi from {:?}", self);
+    }
+}
+
+impl<T: fmt::Debug> SayHi for Box<T> {
+    fn say_hi(self: Pin<&Self>) {
+        println!("Hi from Box<{:?}>", self.get_ref());
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let boxed_value = Box::new(42);
+    let pinned_boxed_value = Pin::new(&boxed_value);
+    pinned_boxed_value.say_hi();
 }
